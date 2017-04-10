665d0a162ea258c3d3117ee580e77032424a4c45
Kevin <kevin.mellachew@gmail.com>
2017-04-10 16:58:38 -0400

    Created main menu

--- client/__main__.py
+++ client/__main__.py
@@ -6,7 +6,9 @@ window = curses.initscr()
 
 # Draw Main Menu
 size = window.getmaxyx()
-window.addstr("curses init")
+window.addstr(round(size[0] / 2) - 3, round(size[1] / 2) - 7, "Single Player")
+window.addstr(round(size[0] / 2) - 1, round(size[1] / 2) - 6, "MultiPlayer")
+window.addstr(round(size[0] / 2) + 1, round(size[1] / 2) - 4, "Options")
 window.refresh()
 window.getch()
 curses.endwin()
