36aec4a8e2d5fcab81edb989213f98d6c538d212
Kevin <kevin.mellachew@gmail.com>
2017-04-12 15:13:12 -0400

    created graphics module

--- client/__main__.py
+++ client/__main__.py
@@ -1,5 +1,6 @@
 # import system
 import curses
+import graphics
 
 # Initialize Screen
 window = curses.initscr()
@@ -7,27 +8,8 @@ window.keypad(1)
 selectMenu = 0
 key = None
 
-
-# Draw Main Menu
-def drawMenu():
-    size = window.getmaxyx()
-    if selectMenu == 0:
-        window.addstr(round(size[0] / 2) - 3, round(size[1] / 2) - 7, "Single Player", curses.A_REVERSE)
-        window.addstr(round(size[0] / 2) - 1, round(size[1] / 2) - 6, "MultiPlayer")
-        window.addstr(round(size[0] / 2) + 1, round(size[1] / 2) - 4, "Options")
-    elif selectMenu == 1:
-        window.addstr(round(size[0] / 2) - 3, round(size[1] / 2) - 7, "Single Player")
-        window.addstr(round(size[0] / 2) - 1, round(size[1] / 2) - 6, "MultiPlayer", curses.A_REVERSE)
-        window.addstr(round(size[0] / 2) + 1, round(size[1] / 2) - 4, "Options")
-    else:
-        window.addstr(round(size[0] / 2) - 3, round(size[1] / 2) - 7, "Single Player")
-        window.addstr(round(size[0] / 2) - 1, round(size[1] / 2) - 6, "MultiPlayer")
-        window.addstr(round(size[0] / 2) + 1, round(size[1] / 2) - 4, "Options", curses.A_REVERSE)
-    window.refresh()
-
-
 while True:
-    drawMenu()
+    graphics.drawMenu(window, selectMenu)
     key = window.getch()
     if key == curses.KEY_UP and selectMenu > 0:
         selectMenu -= 1
