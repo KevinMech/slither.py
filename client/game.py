3e6763d438da2ac6cdfe6dfb23d29b160c54e694
Kevin <kevin.mellachew@gmail.com>
2017-04-17 17:05:29 -0400

    Fixed Alignment display bug in Multiplayer Menu

--- client/game.py
+++ client/game.py
@@ -12,20 +12,26 @@ def _connectServer(window):
     '''Used to connect the client to the server'''
     center = graphics.findCenter(window)
     # Attempt to create a socket
-    window.addstr(center[0], center[1], 'Creating Socket Connection...')
+    window.addstr(center[0] - 2, center[1] - 16, 'Creating Socket Connection...')
     try:
         clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except Exception as e:
-        window.addstr(center[0], center[1], 'Failed to create socket connection :(')
-        window.addstr(center[0] + 1, center[1], str(e))
+        window.clear()
+        window.refresh
+        window.addstr(center[0] - 2, center[1] - 16, 'Failed to create socket connection :(')
+        window.addstr(center[0], center[1] - 16, str(e))
         window.getch()
         window.clear()
     # Attempt to connect to server
-    window.addstr(center[0], center[1], 'Connecting to server...')
+    window.addstr(center[0] - 2, center[1] - 16, 'Connecting to server...')
     try:
+        window.clear()
+        window.refresh
         clientSocket.connect(('127.0.0.1', 35246))
     except Exception as e:
-        window.addstr(center[0] - 2, center[1] - 1, 'Failed to connect to server :(')
-        window.addstr(center[0] + 1, center[1] - 1, str(e))
+        window.clear()
+        window.refresh()
+        window.addstr(center[0] - 2, center[1] - 16, 'Failed to connect to server :(')
+        window.addstr(center[0], center[1] - 16, str(e))
         window.getch()
         window.clear()
