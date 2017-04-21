8e05ac80d919b0c4a3b6d1acf4b7a56172a4235d
Kevin <kevin.mellachew@gmail.com>
2017-04-20 20:38:38 -0400

    Created servers level generator

---
+++ server/game.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+	createGrid: function (width, height) {
+		grid = [];
+		tile = 1;
+		for(x = 0; x < width; x++){
+			row = [];
+			for(y = 0; y < height; y++){
+				row.push(tile);
+				tile++;
+			}
+			grid.push(row);
+		}
+		return grid;
+	}
+};
--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
 const net = require('net');
+const game = require('./game');
 const port = 35246;
 
+//Create Server
+console.log('creating server...');
 const server = net.createServer();
 
+//On discovering new client, create a new socket and attach event handlers to it
 server.on('connection', function (socket) {
-	console.log('found new client!');
+	console.log('found new snake!');
 });
 
+//Create the level
+cosole.log('creating level...');
+const grid = game.createGrid(10,10);
+console.log(grid);
+
+//Start Listening on server
 server.listen(port, function () {
 	console.log('server now listening for clients on port ' + port);
 });
