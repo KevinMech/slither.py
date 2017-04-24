56cc27688b60e58299b1a769592cd66f7fac13c6
Kevin <kevin.mellachew@gmail.com>
2017-04-24 17:19:38 -0400

    Refactored connectServer function
    condensed the two try statements into one and removed unneccessary comments and code

--- client/game.py
+++ client/game.py
@@ -7,39 +7,26 @@ def start(window):
     window.refresh()
     connection = _connectServer(window)
     window.getch()
+    window.clear()
+
 
 def _connectServer(window):
-    '''Used to connect the client to the server'''
+    '''Attempts to create a socket and connects the client to the server'''
     center = graphics.findCenter(window)
-    # Attempt to create a socket
-    window.addstr(center[0] - 2, center[1] - 16, 'Creating Socket Connection...')
+    window.clear()
+    window.addstr(center[0] - 2, center[1] - 16, 'Connecting to server...')
+    window.refresh()
+    connection = ""
     try:
         clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    except Exception as e:
-        window.clear()
-        window.refresh()
-        window.addstr(center[0] - 2, center[1] - 16, 'Failed to create socket connection :(')
-        window.addstr(center[0], center[1] - 16, str(e))
-        window.getch()
-        window.clear()
-    # Attempt to connect to server
-    window.addstr(center[0] - 2, center[1] - 16, 'Connecting to server...')
-    try:
-        window.clear()
-        window.refresh()
+        clientSocket.settimeout(10)
         clientSocket.connect(('127.0.0.1', 35246))
+        connection = clientSocket.recv(1024)
     except Exception as e:
         window.clear()
-        window.refresh()
         window.addstr(center[0] - 2, center[1] - 16, 'Failed to connect to server :(')
         window.addstr(center[0], center[1] - 16, str(e))
-        window.getch()
-        window.clear()
-    clientSocket.settimeout(10)
-    connection = clientSocket.recv(1024)
+        window.refresh()
     if connection == b'connected':
         window.addstr(center[0] - 2, center[1] - 16, 'Found server! Now loading Map...')
-    else:
-        window.addstr(center[0] - 2, center[1] - 16, connection)
     return connection
-    window.getch()
